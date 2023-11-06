1901ac2e3b98c32d22405b009824b23d6a12a729
Daniela Baron <baron.daniela@gmail.com>
2023-11-06 07:54:40 -0500

    render feedback form on retro-feedback slash command and add interactivity endpoint for slack app to post form submission

--- bot/slash_commands.rb
+++ bot/slash_commands.rb
@@ -1,1 +1,2 @@
 require_relative "slash_commands/retro_open"
+require_relative "slash_commands/retro_feedback"
---
+++ bot/slash_commands/retro_feedback.rb
@@ -0,0 +1,98 @@
+SlackRubyBotServer::Events.configure do |config|
+  config.on :command, "/retro-feedback" do |command|
+    team_id = command[:team_id]
+    team = Team.find_by(team_id:)
+    slack_client = Slack::Web::Client.new(token: team.token)
+    channel_id = command[:channel_id]
+    trigger_id = command[:trigger_id]
+    command.logger.info "Received team: #{team.name}, channel_id: #{channel_id}"
+
+    # https://api.slack.com/methods/views.open
+    slack_client.views_open(modal_payload(trigger_id))
+    nil
+  end
+end
+
+# rubocop:disable Metrics/MethodLength
+def modal_payload(trigger_id)
+  {
+    trigger_id:,
+    view: {
+      type: "modal",
+      title: {
+        type: "plain_text",
+        text: "Retrospective Feedback",
+        emoji: true
+      },
+      submit: {
+        type: "plain_text",
+        text: "Submit",
+        emoji: true
+      },
+      close: {
+        type: "plain_text",
+        text: "Cancel",
+        emoji: true
+      },
+      blocks: [
+        {
+          type: "input",
+          block_id: "category_block",
+          element: {
+            type: "static_select",
+            action_id: "category_select",
+            placeholder: {
+              type: "plain_text",
+              text: "Select category"
+            },
+            options: [
+              {
+                text: {
+                  type: "plain_text",
+                  text: "Something we should keep doing"
+                },
+                value: "keep"
+              },
+              {
+                text: {
+                  type: "plain_text",
+                  text: "Something we should stop doing"
+                },
+                value: "stop"
+              },
+              {
+                text: {
+                  type: "plain_text",
+                  text: "Something to try"
+                },
+                value: "try"
+              }
+            ]
+          },
+          label: {
+            type: "plain_text",
+            text: "Category"
+          }
+        },
+        {
+          type: "input",
+          block_id: "comment_block",
+          element: {
+            type: "plain_text_input",
+            action_id: "comment_input",
+            multiline: true,
+            placeholder: {
+              type: "plain_text",
+              text: "Enter your feedback"
+            }
+          },
+          label: {
+            type: "plain_text",
+            text: "Comment"
+          }
+        }
+      ]
+    }
+  }
+end
+# rubocop:enable Metrics/MethodLength
