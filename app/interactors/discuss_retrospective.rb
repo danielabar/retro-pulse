1c4e6b214cc7ff9fdcd13d2cbf03af76021fc77e
Daniela Baron <baron.daniela@gmail.com>
2023-12-28 15:55:21 -0500

    update discuss retro interactor to handle edge cases

--- app/interactors/discuss_retrospective.rb
+++ app/interactors/discuss_retrospective.rb
@@ -2,11 +2,13 @@ class DiscussRetrospective
   include Interactor
   include SlackCommentBuilder
 
-  # TODO: What if there is no open retro?
-  # TODO: What if did not receive any category?
-  # TODO: What if received invalid cateogry?
   def call
     retrospective = Retrospective.open_retrospective.first
+    return no_open_retrospective_message if retrospective.nil?
+
+    category = extract_valid_category
+    return invalid_category_message unless category
+
     comments = retrospective.comments_by_category(category: context.category)
     post_message(comments)
   rescue StandardError => e
@@ -15,6 +17,24 @@ class DiscussRetrospective
   end
 
   private
+
+  def no_open_retrospective_message
+    message = "There is no open retrospective. Please run `/retro-open` to open one."
+    send_error_message(message)
+  end
+
+  def extract_valid_category
+    category = context.category&.to_sym
+    return category if Comment.categories.key?(category)
+
+    nil
+  end
+
+  def invalid_category_message
+    valid_categories = Comment.categories.keys.map(&:to_s).join(", ")
+    message = "Invalid discussion category. Please provide a valid category (#{valid_categories})."
+    send_error_message(message)
+  end
 
   def post_message(comments)
     blocks = build_header_block(comments, Comment.header(context.category))
@@ -30,6 +50,13 @@ class DiscussRetrospective
       channel: context.channel_id,
       text: "fallback TBD",
       blocks:
+    )
+  end
+
+  def send_error_message(text)
+    context.slack_client.chat_postMessage(
+      channel: context.channel_id,
+      text:
     )
   end
 
