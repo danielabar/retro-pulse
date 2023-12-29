95cd9ca1dfc8074b9f44ef6a8e2ebaa6e0775988
Daniela Baron <baron.daniela@gmail.com>
2023-12-29 14:42:51 -0500

    move url building logic out of view and into controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
-  # TODO: Generate slack url here rather than in view
-  def index; end
+  def index
+    @slack_oauth_url = build_slack_oauth_url
+  end
+
+  private
+
+  def build_slack_oauth_url
+    scope = SlackRubyBotServer::Config.oauth_scope_s
+    client_id = ENV.fetch("SLACK_CLIENT_ID", nil)
+    SlackRubyBotServer::Config.oauth_authorize_url + "?scope=#{scope}&client_id=#{client_id}"
+  end
 end
---
+++ spec/requests/welcome_request_spec.rb
@@ -0,0 +1,26 @@
+# spec/requests/welcome_request_spec.rb
+
+require "rails_helper"
+require "cgi"
+
+RSpec.describe "Welcome" do
+  describe "GET /" do
+    it "renders the index template and includes the correct OAuth URL" do
+      get root_path
+
+      # Compare the response body after unescaping HTML entities
+      expect(unescape_html(response.body)).to include(build_expected_oauth_url)
+    end
+
+    def build_expected_oauth_url
+      scope = SlackRubyBotServer::Config.oauth_scope_s
+      client_id = ENV.fetch("SLACK_CLIENT_ID", nil)
+      "#{SlackRubyBotServer::Config.oauth_authorize_url}?scope=#{scope}&client_id=#{client_id}"
+    end
+
+    # Unescape HTML entities in a given HTML string
+    def unescape_html(html)
+      CGI.unescapeHTML(html)
+    end
+  end
+end
