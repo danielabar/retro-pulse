46cb7d282c04e4638ce06c7b44980b30e3f9c9cb
Daniela Baron <baron.daniela@gmail.com>
2024-02-29 08:49:02 -0500

    add some simple seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+# Create Retrospectives
+retrospective = Retrospective.create(title: "Quantum Canvas Sprint 3", status: :open)
+
+# Create Comments for the Retrospective
+Comment.create(
+  content: "Maintaining end-to-end tests has been effective in catching bugs early. " \
+           "Let's keep prioritizing this practice for robust and efficient development.",
+  anonymous: false,
+  retrospective:,
+  category: :keep,
+  slack_user_id: Faker::Alphanumeric.alphanumeric(number: 6),
+  slack_username: Faker::Internet.username
+)
+
+Comment.create(
+  content: "Kudos to the team for successfully implementing the new feature toggle system " \
+           "this sprint! It's been a game-changer in terms of flexibility and risk mitigation. " \
+           "Having the ability to toggle features on and off easily has not only allowed us to " \
+           "ship features more confidently but has also minimized the impact of potential bugs.",
+  anonymous: true,
+  retrospective:,
+  category: :keep
+)
